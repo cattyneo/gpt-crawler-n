7f5aff35dc95caa89696b03845a932251e4398eb
yuyuyu2118 <yuyuyu2118@gmail.com>
2023-11-22 17:48:34 +0900

    settingExample

--- config.ts
+++ config.ts
@@ -5,4 +5,9 @@ export const defaultConfig: Config = {
   match: "https://www.builder.io/c/docs/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
+  waitTime: 1000,
+  onVisitPage: async ({ visitPageWaitTime }) => {
+    await new Promise(resolve => setTimeout(resolve, visitPageWaitTime ?? 1000));
+  },
+  userAgent: "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 };
